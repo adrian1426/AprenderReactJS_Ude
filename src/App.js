5f4cc421cc9f5cec04a2d7837138a874d8f65061
Adrian Hernandez <X193664@la.merckgroup.com>
2019-06-10 15:16:24 -0500

    trabajando con listas

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import './App.css';
-import ConditionalSection from './sections/conditional';
 
 
 function App() {
+  const numbers=[1,3,2,6,8];
+
   return (
     <div className="App">
-    <ConditionalSection/>
+    <h4>Trabajando con listas</h4>
+    {
+      numbers.map((number,i)=>{
+        return (
+        <p key={i}>Soy el número {number}</p>
+      );
+      })
+    }
     </div>
   );
 }
