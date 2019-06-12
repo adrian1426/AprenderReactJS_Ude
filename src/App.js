524a00960c154bdfe6a28f99186c5f3a29bafce9
Adrian Hernandez <X193664@la.merckgroup.com>
2019-06-11 20:03:37 -0500

    chuldren y prpotypes

--- src/App.js
+++ src/App.js
@@ -1,30 +1,45 @@
 import React from 'react';
 import './App.css';
-import Forms from './sections/forms';
+
+class Box extends React.Component{
+  render(){
+    return(
+      <div style={{border:'1px solid #000', margin:5, padding:5}}>
+        {this.props.children}
+      </div>
+    );
+  }
+}
+
+class Article extends React.Component{
+  render(){
+    return(
+      <section>
+        <h2>{this.props.title}</h2>
+        {this.props.author && <p><em>Escrito por: {this.props.author}</em></p>}
+        <Box>{this.props.date}</Box>
+        <article>
+          {this.props.children}
+        </article>
+      </section>
+    );
+  }
+}
 
 class App extends React.Component{
-  constructor(){
-    super();
-    this.state={
-      mouseX:0,
-      mouseY:0
-    }
-  }
-
-  handleclick(e){
-   console.log("e: ",e);
-   console.log("e: ",e.nativeEvent);
-  }
-
-  handleMouseMove = (e) =>{
-    const {clientX, clientY} = e;
-    this.setState({mouseX:clientX,mouseY:clientY});
-  }
-
   render(){
     return (
       <div className="App">
-      <Forms/>
+      <h4>Children props</h4>
+      <Box>Hola soy un children</Box>
+      <Box>Hola soy un children 2</Box>
+
+      <Article 
+      date={new Date().toLocaleDateString()}
+      title="Articulo sobre porp children">  
+      <p>El contenido quwe envolvemos por porps children</p>
+      <strong>El contenido quwe envolvemos por porps children</strong>
+      </Article>
       </div>
     );
   }
