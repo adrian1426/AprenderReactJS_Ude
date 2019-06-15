5272c0b36ce845babda94d4cfe131e9741f7fc7c
Adrian Hernandez <X193664@la.merckgroup.com>
2019-06-15 00:22:20 -0500

    ciclo de vida- constructor - montaje

--- src/App.js
+++ src/App.js
@@ -1,45 +1,17 @@
 import React from 'react';
 import './App.css';
 
-class Box extends React.Component{
-  render(){
-    return(
-      <div style={{border:'1px solid #000', margin:5, padding:5}}>
-        {this.props.children}
-      </div>
-    );
-  }
-}
-
-class Article extends React.Component{
-  render(){
-    return(
-      <section>
-        <h2>{this.props.title}</h2>
-        {this.props.author && <p><em>Escrito por: {this.props.author}</em></p>}
-        <Box>{this.props.date}</Box>
-        <article>
-          {this.props.children}
-        </article>
-      </section>
-    );
-  }
-}
-
 class App extends React.Component{
+  constructor(props){
+    super(props);
+    this.state={mensajeInicial:'mensaje inicial'}
+  }
+
   render(){
     return (
       <div className="App">
-      <h4>Children props</h4>
-      <Box>Hola soy un children</Box>
-      <Box>Hola soy un children 2</Box>
-
-      <Article 
-      date={new Date().toLocaleDateString()}
-      title="Articulo sobre porp children">  
-      <p>El contenido quwe envolvemos por porps children</p>
-      <strong>El contenido quwe envolvemos por porps children</strong>
-      </Article>
+      <h4>Ciclo de "Montaje" - "Construcor"</h4>
+      {this.state.mensajeInicial}
       </div>
     );
   }
